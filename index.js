37416e5a2f19f2092cde307d7bc471f1f8dc15b0
SABARUNABIMAS <142733504+SABARUNABIMAS@users.noreply.github.com>
2025-12-05 13:25:26 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+const express = require("express"); 
+const app = express(); 
+app.get("/", (req, res) => res.send("CI/CD Cloud Deployment Successful!")); 
+app.listen(3000, () => console.log("Server running on port 3000"));
